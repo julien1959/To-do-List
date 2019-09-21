0add537214062c0e9d44790c1321e6e3a354012d
Julien <dev.julien.f@gmail.com>
2019-09-21 15:00:44 +0200

    creating the html folder and js

---
+++ script.js
@@ -0,0 +1,38 @@
+const userInput = document.body.querySelector("userinput");
+const enter = document.body.querySelector("enter");
+const ul = document.body.querySelector("ul");
+let = li;
+
+function getInputValueClick() {
+
+}
+
+function getInputValueEnter() {
+
+}
+
+function createNewLi() {
+   li =  document.body.ul.createElement("li")
+}
+
+function insertInputInLi() {
+
+}
+
+function insertNewLiInDom() {
+
+}
+
+function addNewLiInToDoList() {
+    createNewLi();
+    insertInputInLi();
+    insertNewLiInDom();
+}
+
+
+function() {
+    alert;
+}
+
+
+// element.addEventListener("quoi?"(click-keypress..) , Action(fonction));
